85198d5dad6f5915b3e7a3dc9b12cab44d8e4b0b
Mangesh Gaherwar <mangesh.gaherwar@smartlockr.eu>
2022-09-09 15:24:14 +0530

    Add project files.

---
+++ GrpcOnAzure/Program.cs
@@ -0,0 +1,25 @@
+using GrpcOnAzure.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(opt =>
+{
+    opt.ListenAnyIP(4000);
+    opt.ListenAnyIP(4200, options =>
+    {
+        options.Protocols = HttpProtocols.Http2;
+    });
+});
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
